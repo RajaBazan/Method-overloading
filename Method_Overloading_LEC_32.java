fbdad38839ff4cbedc9df77bd79f1675c1e1fb65
Muhammad Bazan <112274805+RajaBazan@users.noreply.github.com>
2023-08-17 02:28:19 +0500

    Add files via upload

---
+++ Method_Overloading_LEC_32.java
@@ -0,0 +1,46 @@
+package hello.com;
+
+//public class Method_Overloading_LEC_32 {
+//    // Case 2: (Static void body)(Changing Integer)
+//    static void change(int a){
+//        a = 13;
+//    }
+//
+//    // Case 3: (Static void body)(Changing array)
+//        static void change2(int[] arr){
+//        arr[3] = 98;
+//    }
+//    //Case1(Tell Joke): using void by tell joke
+//   static void main()
+//   {
+//       System.out.println("hey");}
+//
+//    public static void main(String[] args) {
+//
+//        main();
+//        // Case:2 By using change void by changing integer
+//        int [] marks = {10, 12, 13, 14, 15};
+//    int x = 23;
+//        System.out.println("The value of x is after change affect:" + x);
+////      Case 3 : Changing arrays
+//        int[] marks = { 10, 13, 14, 15};
+//        change2(marks);
+//        System.out.println("The value of x after changing in arrays:" + marks[2]);
+//    }}
+////    Method Overloading Case
+////    static void foo(){
+////        System.out.println("Good Morning Bazan");
+////
+////    }
+////
+//////         In the foo we can use anything which you want to give a name of that:
+////    static void foo(int a, int b){      // Where int, int b are perameters
+////        System.out.println("Good Morning " + a + " Bazan");
+////        System.out.println("Good Morning " + b + " Bazan");
+////    }
+////public static void main(String[] args) {
+////foo();
+////foo(3000, 4000);// These are arguments that use in language // Arguments are accutual!
+////
+////}
+////}
